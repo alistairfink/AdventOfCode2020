5eb549709a236b997efcfa533f841451631487c0
Alistair Fink <alistairfink@gmail.com>
2020-12-03 10:54:30 -0500

    Challenge 1

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ For Advent of Code challenges at adventofcode.com
 ## Usage
 Run the following with a challenge number.
 ```
-go run main.go [Challenge Number]
+go run main.go [Challenge Number] [Use Test Data (True/False)]
 ```
 
 
 ## Challenge Numbers
+1 - Report Repair - https://adventofcode.com/2020/day/1
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/alistairfink/AdventOfCode2020/Challenges"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 var NAME string
@@ -11,15 +12,21 @@ var NAME string
 func main() {
 	args := os.Args
 	NAME = formatName(args[0])
-	if len(args) != 2 {
+	if len(args) != 3 {
 		printInvalid()
 		return
 	}
 
 	challengeNumber := args[1]
+	useTestData, err := strconv.ParseBool(args[2])
+	if err != nil {
+		panic(err)
+	}
 
 	if challengeNumber == "test" {
 		Challenges.ChallengeTest()
+	} else if challengeNumber == "1" {
+		Challenges.Challenge1(useTestData)
 	} else {
 		println("Invalid Challenge Number:", challengeNumber)
 	}
@@ -40,5 +47,5 @@ func formatName(input string) string {
 
 func printInvalid() {
 	println("Invalid Command.")
-	println("Usage:", NAME, "[Challenge Number]")
+	println("Usage:", NAME, "[Challenge Number]", "[Use Test Data (true/false)]")
 }
